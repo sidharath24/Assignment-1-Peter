75afa0536d86ce59ca25a951909d66d67e362da5
visualstudio <visualstudio@124-PC18>
2019-02-15 18:53:26 -0500

    another commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,4 +12,52 @@ namespace ConsoleApp2
         {
         }
     }
+    class UsingRecursion
+    {
+        // Create an application which 
+        // calculates the sum of all the numbers from n to m recursively:
+
+        public static int CalculateRecursively(int n, int m)
+        {
+            int sum = n;
+
+            if (n < m)
+            {
+                n++;
+                return sum += CalculateRecursively(n, m);
+            }
+            return sum;
+        }
+
+
+    }
+
+    class CountrySide
+    {
+        // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+        Village Maeland;
+        Village Helmholtz;
+        Village Alst;
+        Village Wessig;
+        Village Badden;
+        Village Uster;
+        Village Schvenig;
+
+
+        class Village
+        {
+            public Village(string _villageName, bool _isAHere)
+            {
+                isAstrildgeHere = _isAHere;
+                VillageName = _villageName;
+            }
+
+            public Village west;
+            public Village east;
+            public string VillageName;
+            public int distanceToNextVillage;
+            public int distanceToPreviousVillage;
+            public bool isAstrildgeHere;
+        }
+    }
 }
